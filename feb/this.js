99f9a0fddbc4c9d752e3d40cdbf2ea693c0403e8
Ken Zhang <kyz211@nyu.edu>
2016-03-15 03:41:53 -0400

    commit

---
+++ feb/this.js
@@ -0,0 +1,13 @@
+var name = 'john'
+var obj = {
+    name: 'bob',
+    property: {
+        name: 'jane',
+        getName: function() {
+            return this.name
+        }
+    } 
+}
+console.log(obj.property.getName())
+var test = obj.property.getName
+console.log(test())

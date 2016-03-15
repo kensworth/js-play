f268650bfdf6044f4deb2d270fe6c29c3de99528
Ken Zhang <kyz211@nyu.edu>
2016-03-15 19:31:47 -0400

    function wrapping, plus using Array.prototype

--- feb/reverse-arr-in-place.js
+++ feb/reverse-arr-in-place.js
@@ -1,8 +1,9 @@
+(function() {
 'use strict'
 //reverse an array of integers in place
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-let reverse = arr => {
+Array.prototype.reverse = Array.prototype.reverse || function(arr) {
     for(let i = 0; i < arr.length / 2; i++) {
         let opp =  arr.length - i - 1   
         arr[i] += arr[opp]
@@ -12,5 +13,6 @@ let reverse = arr => {
 }
 
 
-reverse(arr)
+arr.reverse()
 arr.map((n) => console.log(n))
+})()

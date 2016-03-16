5c71ca71230bbc06d9a548c1eba1b92932631288
Ken Zhang <kyz211@nyu.edu>
2016-03-16 19:48:41 -0400

    hi master yoda

--- feb/callback-apply.js
+++ feb/callback-apply.js
@@ -1,7 +1,11 @@
 (function() {
-    (firstName, lastName, callback, callbackObj) => {
-        // do stuff
+    'use strict'
+    let func = (firstName, lastName, callback, callbackObj) => {
+    // do stuff
+    console.log('doing stuff')  
+    callback.apply(callbackObj, [firstName, lastName])
+    callback.call(callbackObj, firstName, lastName)
     }
-    //callback.apply(callbackObj, [firstName, lastName])
-    //callback.call(callbackObj, firstName, lastName)
+    let callbackFunc = (firstName, lastName) => console.log('hi ' + firstName + ' ' + lastName)
+    func('master', 'yoda', callbackFunc, {})
 })()

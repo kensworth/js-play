92cdc792e2f09c7a7a2fef6d97d25efdba004ce2
Ken Zhang <kyz211@nyu.edu>
2016-06-04 13:26:05 -0400

    commit

---
+++ etc/hoist-test.js
@@ -0,0 +1,7 @@
+console.log(test2)
+function test2() {
+	return 'hello2'
+}
+console.log(test3)
+test = _ => 'hello3' 
+// fat arrow is not hoisted the same way an anonymous function is. It behaves like a named function.
